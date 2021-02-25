21063872cc62a58c9ed8a5196a52d5eade092bce
francizjamez <francizjamez@gmail.com>
2021-02-26 00:23:19 +0800

    responsive image

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,6 +4,7 @@ const Body = () => {
       <img
         src="https://www.thespruceeats.com/thmb/EfrUsdATrAjnDVAJJ9LhromYIok=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/bubble-tea-recipe-694162-hero-02-e428d92163814642903b12c5ac14de24.jpg"
         id="bubbletea"
+        class="body-img"
       ></img>
       <h2>Our products are made from premium ingredients</h2>
     </div>
